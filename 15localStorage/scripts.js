2c588d7a5930ade1fbd2e41d4640daa3aebf5fce
Alexandru Loghin <alexandru.loghin@endava.com>
2019-05-10 11:54:40 +0300

    day 15 done

---
+++ 15localStorage/scripts.js
@@ -0,0 +1,43 @@
+/*jshint esversion:6*/
+
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+function addItem(e) {
+	e.preventDefault();
+	const text = (this.querySelector('[name=item]')).value;
+	const item = {
+	text,
+	done: false
+	};
+
+	items.push(item);
+	populateList(items, itemsList);
+	localStorage.setItem('items', JSON.stringify(items));
+	this.reset();
+}
+
+function populateList(plates = [], platesList) {
+	platesList.innerHTML = plates.map((plate, i) => {
+		return `
+			<li>
+				<input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+				<label for="item${i}">${plate.text}</label>
+			</li>
+		`;
+	}).join('');
+}
+
+function toggleDone(e) {
+	if(!e.target.matches('input')) return;
+	const index = e.target.dataset.index;
+	items[index].done ^= true;
+	localStorage.setItem('items', JSON.stringify(items));
+}
+
+window.onload = (function() {
+	populateList(items, itemsList);
+	addItems.addEventListener('submit', addItem);
+	itemsList.addEventListener('click', toggleDone);
+})();
